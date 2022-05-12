f5dfd965f23832bffaa88fd73d3b2cfbd002ef1d
Gloria <gloedem@gmail.com>
2022-05-12 02:21:09 -0400

    update yaml - fix db setup command
    update yaml - update db setup commands
    
    fixed type in root route
    
    autogenerated coverage file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Change root  
-  root 'article#index'
+  root 'articles#index'
   
 
 end
