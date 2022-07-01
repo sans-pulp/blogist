17dcedcbee653129007305af8c0bdfb301972301
Gloria <gloedem@gmail.com>
2022-07-01 01:56:32 -0400

    Revert "add slug column to articles table, run migration"
    This reverts commit 9059a05ef05bcd7198b055ee8228f43e70254925.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_25_190308) do
+ActiveRecord::Schema.define(version: 2022_06_05_030345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2022_06_25_190308) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "slug"
   end
 
 end
