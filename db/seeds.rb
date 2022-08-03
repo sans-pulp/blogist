eb7ed1316872ba40d6d91f3e7ff65560e7432cc7
Gloria <gloedem@gmail.com>
2022-08-02 23:13:00 -0400

    seed db using Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#  Create 50 articles
+
+50.times do |n|
+  title = Faker::Hipster.sentence(word_count: 2, random_words_to_add: 0, open_compounds_allowed: false)
+  body = Faker::Hipster.paragraph_by_chars(characters: 100)
+  Article.create!(title: title, body: body)
+end
